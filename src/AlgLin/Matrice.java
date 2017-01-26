fe64b1a2581aace77cc1d41ab48c7c7345342158
maitmansour <mohamedaitmansour95@gmail.com>
2017-01-26 10:29:25 +0100

    TP2 - Creation de vecteur

--- src/AlgLin/Matrice.java
+++ src/AlgLin/Matrice.java
@@ -10,6 +10,12 @@ public class Matrice {
 	//Les attributs a definir
 	private double coefficient[][];
 	
+	public double[][] getCoefficient() {
+		return coefficient;
+	}
+	public void setCoefficient(double[][] coefficient) {
+		this.coefficient = coefficient;
+	}
 	public Matrice(int nblignes, int nbcolonnes) {
 		System.out.println("CONSTRUCTEUR : public Matrice( "+ nblignes+", "+nbcolonnes+")");
 		coefficient= new double[nblignes][nbcolonnes];
@@ -144,25 +150,137 @@ public class Matrice {
 		return resultat;
 	}
 	
-	public static Matrice multiplicationMatrices(Matrice m1, Matrice m2){
-		Matrice resultat = new Matrice(m1.coefficient.length,m1.coefficient[0].length );
-    // Initialisation de tableau
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				resultat.coefficient[i][j] = 0.00000;
+	public static Matrice multiplicationMatrices(Matrice m1, Matrice m2) throws Exception{
+		if (m1.getCoefficient().length==m2.getCoefficient().length){
+			if (m1.getCoefficient()[0].length==m2.getCoefficient()[0].length){
+				Matrice resultat = new Matrice(m1.coefficient.length,m1.coefficient[0].length );
+			    // Initialisation de tableau
+					for (int i = 0; i < 2; i++) {
+						for (int j = 0; j < 2; j++) {
+							resultat.coefficient[i][j] = 0.00000;
+						}
+					}
+					
+			    //calcul 
+					for (int i = 0; i < m1.coefficient.length; i++) {
+						for (int j = 0; j < m1.coefficient[0].length; j++) {
+							for (int k = 0; k < m1.coefficient[0].length; k++) {
+
+								resultat.coefficient[i][j]+=m1.coefficient[i][k]*m2.coefficient[k][j];
+							}
+						}
+					} 
+					return resultat;
 			}
+
 		}
 		
-    //calcul 
-		for (int i = 0; i < m1.coefficient.length; i++) {
-			for (int j = 0; j < m1.coefficient[0].length; j++) {
-				for (int k = 0; k < m1.coefficient[0].length; k++) {
-
-					resultat.coefficient[i][j]+=m1.coefficient[i][k]*m2.coefficient[k][j];
-				}
-			}
-		} 
-		return resultat;
+		throw new Exception("MULTUPLICATION IMPOSSIBLE - MATRICES NON EGAUX");
+		
+		
 	}
+	
+	public static void main(String[] args) {
+		select();
+	}
+	
+	public static void showmenu(){
+		System.out.println("===========================================");
+		System.out.println("Menu des testes  possibles sur les matrices");
+		System.out.println("===========================================");
+		System.out.println("0.Creation des matrices ");
+		System.out.println("1.Affihage des matrices");
+		System.out.println("2.Afficher Nombre d'element d'une matrice");
+		System.out.println("3.Produit Scalaire avec une matrice (ex. 5.2)");
+		System.out.println("4.Recuperer l'element situe a M1[2][2]");
+		System.out.println("5.Remplacer l'element situe a M1[2][2] par 5.3");
+		System.out.println("6.Addition deux matrices");
+		System.out.println("7.Multiplication deux matrices");
+		System.out.println("8.exit");		
+	}
+	
+
+
+	public static void select(){
+		int nb,x,val, pos, ch,yes=0;
+		String str;
+		Matrice m1 = null;
+		Matrice m2 = null;
+		Scanner sc=new Scanner(System.in);
+		try{
+			while(yes==0){
+				showmenu();
+
+				System.out.println("\n \n \n \n");
+				System.out.print("Entrer votre choix :");
+				ch=sc.nextInt();
+				switch(ch){
+					case 0:
+					m1 = new Matrice("//home//etudiant//am163898//Bureau//filename2.txt");
+					m2 = new Matrice("//home//etudiant//am163898//Bureau//filename22.txt");
+
+					break;
+					case 1:
+					System.out.println(m1.toString()); 
+					System.out.println("\n \n"); 
+					System.out.println(m2.toString()); 
+					break;
+					case 2:
+					
+	//Nombre element d'une matrice
+					System.out.println("Nombre elements M1"); 
+					m1.NbElements();
+
+					break;
+					case 3:
+					
+	//Produit Scalaire avec une matrice
+					System.out.println("Produit Scalaire m1*5.2"); 
+					m1.produitScalaire(5.2);
+					System.out.println(m1.toString()); 
+					
+					break;
+					case 4:
+					
+    //R�cup�rer l'element situe a M1[2][2]
+					System.out.println("R�cup�rer l'element situe a M1[2][2]= "+m1.getElement(2, 2)); 
+
+					break;
+
+					case 5 :
+
+	//Remplacer l'element situe a M1[2][2]
+					m1.replaceElement(2, 2,5.3); 
+					break;
+					case 6:
+	//Addition deux matrices
+					Matrice m3 = Matrice.additionMatrices(m1, m2);
+					System.out.println("Addition deux Matrices"); 
+					System.out.println(m3.toString()); 
+					break;
+					case 7:
+
+	//Multiplication deux matrices
+					System.out.println("Multiplication deux Matrices"); 
+					Matrice m4 = Matrice.multiplicationMatrices(m1, m2);
+					System.out.println(m4.toString()); 
+					break;
+					case 8:System.exit(0);
+					default: System.out.println("Invalid choice!");
+
+				}
+
+
+				System.out.print("Continue? Press 0 to continue:");
+				yes=sc.nextInt();
+
+			}
+
+		}catch(Exception e){System.exit(100);}
+
+	}
+
+
+
 
 }
---
+++ src/AlgLin/Vecteur.java
@@ -0,0 +1,73 @@
+package AlgLin;
+
+import java.util.Arrays;
+
+public class Vecteur extends Matrice{
+
+	public Vecteur(int nblignes) {
+		super(nblignes,1);
+	}
+
+	public Vecteur(double [] tmpTable) {
+		this(tmpTable.length);
+		for (int i = 0; i < tmpTable.length; i++) {
+			replaceElement(i, 0, tmpTable[i]);
+		}
+		
+	}
+	
+
+	public Vecteur(String FILENAME) {
+		super(FILENAME);
+	}
+	
+	public int renvoieTaille(){
+		return getCoefficient().length;
+	}
+	
+	double getElement(int emplacement) {
+		return super.getElement(emplacement, 0);
+	}
+	
+	void replaceElement(int emplacement, double element) {
+		super.replaceElement(emplacement, 0, element);
+	}
+	
+	public static double produitScalaire(Vecteur v1, Vecteur v2) throws Exception {
+		double somme = 0.00000;
+		if (v1.renvoieTaille()==v2.renvoieTaille()) {
+			for (int i = 0; i < v1.getCoefficient().length; i++) {
+				somme+=v1.getElement(i)*v2.getElement(i);
+
+			}
+			return somme;
+			
+		}
+		throw new Exception("PRODUIT NON EFFECTUE - TAILLE INCOMPATIBLE");
+	}
+	
+	public static void main(String[] args) {
+		Vecteur v1 = new Vecteur("//home//etudiant//am163898//Bureau//filename4.txt");
+		//System.out.println(v1.toString()); 
+		double[] tmp = {9,8,7,6,5,4,3,2,1};
+		Vecteur v2 = new Vecteur(tmp);
+		System.out.println(v2.toString()); 
+		System.out.println("taille de vecteur : "+v2.renvoieTaille()); 
+		System.out.println("Element situe a 2 : "+v2.getElement(2)); 
+		v2.replaceElement(2, 5);
+		System.out.println("Element situe a 2 apres remplacement: "+v2.getElement(2)); 
+		System.out.println("\n \n Affichage de vecteur : \n"+v2.toString()); 
+		try {
+			System.out.println("\n \n Produit scalaire des deux vecteurs v1*v2 :"+produitScalaire(v1, v2));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+
+		
+
+	}
+
+
+	
+
+}
