71f82695ee50e1483b9987e246d0e498966ee0d5
am163898 <mohamed.ait-mansour@etu.univ-lehavre.fr>
2017-02-16 10:30:49 +0100

    reso

--- src/AlgLin/SysDiagonal.java
+++ src/AlgLin/SysDiagonal.java
@@ -11,8 +11,12 @@ public class SysDiagonal extends SysLin{
 
 	@Override
 	public Vecteur Resolution() {
-		// TODO Auto-generated method stub
-		return null;
+		double solution[] = new double[getOrdre()];
+		for (int i = 0; i < getOrdre(); i++) {
+			 solution[i]=getMatriceSystem().getElement(i, i)*getSecondMembre().getElement(i, 0);
+
+		}
+		return new Vecteur(solution);
 	}
 	
 
@@ -21,9 +25,21 @@ public class SysDiagonal extends SysLin{
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Matrice matrice = new Matrice("//home//etudiant//am163898//Bureau//filename5.txt");
+		Vecteur vecteur = new Vecteur("//home//etudiant//am163898//Bureau//filename4.txt");
+		try {
+			SysDiagonal Diagonal = new SysDiagonal(matrice,vecteur);
+			Vecteur solution = Diagonal.Resolution();
+			System.out.println(solution);
+		} catch (IrregularSysLinException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 
 	}
 
 
 }
+ 
